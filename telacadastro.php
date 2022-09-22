033ae88cc59f074ae78b5a45c2b949d3602b56f9
laianehoger <55981086+laianehoger@users.noreply.github.com>
2022-09-22 08:59:36 -0300

    Update telacadastro.php

--- telacadastro.php
+++ telacadastro.php
@@ -4,16 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="estilo.css">
-    <!-- Bootstrap CSS 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
-    <title>COMPARA! - cadastro</title>
+    <!-- Bootstrap CSS -->
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <title>COMPARA!</title>
 </head>
 <body class="body">
     <div class="container">
-        <div >
+        <div>
             <img src="logo.png" style="width: 250px; display:block; margin: 0px auto; margin-bottom: 20px;">
         </div>
-        <div class="row justify-content-center align-items-center inner-row" style="margin-top: -161px;">
+        <div class="row justify-content-center align-items-center" >
             <div class="col-auto col-lg-5 col-md7">
                 <div class="form-box registration-form p-md-5 p-3">
                     <div class="form-title">
@@ -21,7 +21,7 @@
                     </div>
                     <form method="POST" action="">
                         <div class="form-floating mb-3">
-                            <input type="text" name="nome" class="form-control form-control-sm" placeholder="Nome" id="floatingInput" required>
+                            <input type="text" name="nome" class="form-control form-control-sm" placeholder="Nome" id="floatingInput" >
                             <label for="floatingInput">Nome</label>
                         </div>
                         <div class="form-floating mb-3">
@@ -37,25 +37,35 @@
                             <label for="floatingPassword">Senha</label>
                         </div>
                         <div class="mt-3">
-                            <button type="submit" class="primaryBg text-white" value="submit">Cadastro</button>
+                            <button type="submit" class="primaryBg text-white" value="enviar">Cadastro</button>
                         </div>
                     </form>
                     <div class="mt-3">
-                        <span>Já possui uma conta? Faça </span><button class="p-0 border-0 bg-transparent primaryColor login-show">Login</button>
+                        <span>Já possui uma conta? Faça </span><a href="telalogin.php"><button class="p-0 border-0 bg-transparent primaryColor">Login</button></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+    
+   
+    <!--  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>-->
+ <!--   <script>
+        $('.singup-show').click(function(){
+            $('.registration-form').show();
+            $('.login-form').hide();
+        })
+
+        $('.login-show').click(function(){
+            $('.login-form').show();
+            $('.registration-form').hide();
+        })
+    </script>  -->
 
 
-    <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>-->
 </body>
 </html>
