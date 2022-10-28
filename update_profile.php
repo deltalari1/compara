11bc192269f520fe3f2e399cf57611d5b9c36da3
laianehoger <55981086+laianehoger@users.noreply.github.com>
2022-10-27 21:17:35 -0300

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -57,7 +57,7 @@ if(isset($_POST['update_profile'])){
    <title>Atualizando Perfil</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
