fe56b64275cd41d7f9e6c57ef251cd784342ce0c
Larissa <55930794+deltalari1@users.noreply.github.com>
2022-11-30 00:23:29 -0300

    Delete perfil directory

--- perfil/curtir.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".fa-heart").click(function(){
-        $(this).toggleClass("fa-regular fa-solid");
-        $(this).toggleClass("text-dark text-danger");
-    });
-});
