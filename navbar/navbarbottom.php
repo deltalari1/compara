ec1cb2f5090270d3657ce9f18b2b457d7103478a
Larissa <55930794+deltalari1@users.noreply.github.com>
2022-11-29 23:35:01 -0300

    Add files via upload

---
+++ navbar/navbarbottom.php
@@ -0,0 +1,15 @@
+<nav class="navbar fixed-bottom navbar-dark bg-custom d-block d-md-none fundo-customizado">
+	<div class="container-fluid justify-content-evenly p-1">
+		<a class="nav-link" href="#" role="button">
+			<i class="fa-solid fa-house fa-2x text-white"></i>
+		</a>
+		
+		<a class="nav-link" href="#" role="button">
+			<i class="fa-solid fa-circle-plus fa-2x text-white"></i>
+		</a>
+
+		<a class="nav-link" href="/compara/perfil/perfil.php?usuario=1" role="button">
+			<i class="fas fa-user-alt fa-2x text-white"></i>
+		</a>
+	</div>
+</nav>
